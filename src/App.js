7bfbf4f7adcf7e53a942c23a44f63211ce4499b3
oda <dv@kappacoding.co.uk>
2022-03-06 17:29:00 +0000

    trying to fix a bug whereby my main css file, app.css, is being overwritten by skeleton.css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+// import "./App.css";
 import { Outlet } from "react-router-dom";
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
