1febcb100a590afa599700bba3d9c9171e33e12b
oda <ooddaa@gmail.com>
2022-03-23 12:25:22 +0000

    added git stash & open source posts

--- src/components/pages/Blog/components/posts.js
+++ src/components/pages/Blog/components/posts.js
@@ -206,33 +206,161 @@ def get_prices(tickers, attach_prices=False): ...`}
   {
     id: 2,
     routeName: "blogpost2",
-    header: "Post 2",
-    subheader: "post 2 subheader",
-    dateCreated: [2022, 2, 10],
-    author: "hehe",
+    header: "Git stash",
+    subheader: "Haven't thought it through before making changes on this branch? No biggie - git stash it!",
+    dateCreated: [2022, 3, 5],
+    author: "oda",
+    timeToRead: "3min",
+    timeToThink: "3min",
+    tags: [
+      "git"
+    ],
     body: (
       <div className=".blog-post-content-body">
+
         <p>
-          {reverseString(`Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum officia
-          cupiditate nemo! Vel inventore, nam aliquid corrupti totam dignissimos
-          unde ad non ducimus cum minus, beatae magni praesentium sed velit!`)}
+          <strong>TL/DR: </strong>If you write code first, and think later learn to use these babies:
+          <ul>
+            <li><code>git stash</code></li>
+            <li><code>git stash list</code></li>
+            <li><code>git stash pop</code></li>
+          </ul>
+          
         </p>
+
+        <h3>
+          Mein Workflow
+        </h3>
+        <p>
+          Sorry for the title, couldn't resist. I do struggle with keeping my coding organised - not least because I haven't really coded as part of any large team with strict coding practices/guidelines. So my projects are fair game for me - I can touch any part of it whenever I want. 
+        </p>
+
+        <p>Which more often than not creates a lot of unnecessary code re-writing. Or, even more often, after I wrote something I realise that it logically belongs to another branch.</p>
+
+        <p>If there is no other branch, I simply 
+          <pre>
+            <code>
+            {`$ git checkout -b newBranch
+$ git add .
+$ git commit -m "stuff that should be on newBranch"`}
+            </code>
+          </pre> 
+        </p>
+
+        <p>If there IS the other branch that logically owns the changes, no biggie, <code>git stash</code> to the rescue!  
+          <pre>
+            <code>
+            
+            {`$ git stash     // all changes on current branch are 
+                // reverted, equals to git reset --hard
+$ git checkout anotherBranch
+$ git stash pop // there might be some conflicts for manual resolution, 
+                // I accept all incoming (stashed) changes
+$ git add .
+$ git commit -m "stuff that should have been on anotherBranch"`}
+            </code>
+          </pre> 
+        </p>
+        <p>
+        Voila!
+
+Now we have appropriately positioned and accounted for our code.
+        </p>
+
       </div>
     ),
   },
   {
     id: 3,
     routeName: "blogpost3",
-    header: "Post 3",
+    header: "How I stopped worrying and started to love open source",
     subheader: "post 3 subheader",
-    dateCreated: [2022, 3, 11],
-    author: "Santa",
+    dateCreated: [2022, 3, 23],
+    author: "oda",
+    timeToRead: "3min",
+    timeToThink: "3min",
+    tags: [
+      "open source",
+      "GitHub",
+      "BitBucket",
+      "git push"
+    ],
     body: (
       <div className=".blog-post-content-body">
-        <p>Posto numero 3</p>
+        <p>
+        <strong>TL/DR: </strong>Transition between hoarding behaviour and open source (in my case vs private Bitbucket and public GitHub) - takes a mindshift. The question boils down to: Do I want to be a better developer/coder/programmer/professional? If the answer is YES - go public. If the answer is NO - stick with a private repo.
+        </p>
+
+        
+        
+
+        <h3>Public vs Private</h3>
+        <p>Since the beginning of my coding hobby, in 2012, I was under the impression that what I code matters.
+          Not that I would be able to put my finger on how exactly it mattered or why. Or why I was so sure that anyone would ever spend any of their valuable time reading/stealing/using my precious code. 
+        </p>
+        <p>
+          Uploading my work to a public place changes how I think about the quality of my work. I have three rules: 
+          <ol>
+            
+            <li><strong>To push is better than to hoard.</strong> Pushing to main branch forces me to focus on solving an issue within the restricted timespan (I like those green commits, I do). But at the same time I realise I don't want to push garbage, so a this becomes a battle against <strong>Unrelenting Standards</strong> (ie "it's better now and imperfect that perfect and never").</li>
+            
+            <li><strong>To write documentation is better than write code.</strong> I want my work to be something that I am not ashamed of. The only sure way to get there for me is to incrementally improve my code (logic, organization, comments). Since "incrementally" means "step by step over time" - I ran into the old <strong>WHAT_THE_HELL_IS_THIS_DOING_HERE</strong> problem. When it takes me more time to remember what I was trying to achieve than actually incrementing the quality. Hence I make myself spend time on adding code comments, documentation, and notes. And if I'm completely honest, the benefit that I get when I actually think of what I am trying to achive via code far outweighs the benefits of having  banged out some code for the sake of code lines (what?). </li>
+            
+            <li><strong>To compete is better that to delude yourself.</strong> This is the big one for me. I think open source allows ideas to compete. Even if they compete only within my own head, because, let's get real for a sec, no one is going to go through my repos with any level of attention. If they do, I'd be chaffed to bits to listen to their comments (after a couple of deep breaths that is). Hoarding code in my drawer kills any opportunity for it to be grown into something usable by actual humans. Pushing it out there in the public gives it at least a chance (however small). There is lots of work to do after just simple <code>git push</code> but without the first step, there won't be any others.</li>
+          </ol>
+        </p>
+
+        <h3>Conclusion</h3>
+        <p>
+          Pushing code to a public repo (and do remove all PID - privately identifiable data from your repo) vs hoarding all in a private repo gives you a chance and a stimulus to improve your work. 
+        </p>
       </div>
     ),
   },
+  {
+    id: 4,
+    routeName: "blogpost4",
+    header: "Future posts",
+    subheader: "What I need to write about",
+    dateCreated: [2022, 3, 23],
+    author: "oda",
+    timeToRead: "3min",
+      timeToThink: "3min",
+      tags: [
+      ],
+    body: (
+      <div className=".blog-post-content-body">
+        <ol>
+          <li>find library that styles html code - mine looks horrible and takes forever to add</li>
+          <li>write a bunch of <i>1min solutions</i> based on the problems & solutions from my google sheets</li>
+          <li>add links</li>
+        </ol>
+        <p>
+         
+        </p>
+      </div>
+    ),
+  }
 ];
 
 export default posts;
+
+const stub = {
+  id: 2,
+  routeName: "blogpost2",
+  header: "Post 2",
+  subheader: "post 2 subheader",
+  dateCreated: [2022, 2, 10],
+  author: "oda",
+  timeToRead: "3min",
+    timeToThink: "3min",
+    tags: [
+    ],
+  body: (
+    <div className=".blog-post-content-body">
+      <p>
+       
+      </p>
+    </div>
+  ),
+}
