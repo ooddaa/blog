67d9eadb8624508492525dca1fdc096ce842357d
oda <ooddaa@gmail.com>
2022-05-17 17:36:59 +0100

    navbar-links position fixed on scroll with useEffect. there must be a simpler way to do this

--- src/components/pages/Welcome/Welcome.tsx
+++ src/components/pages/Welcome/Welcome.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import AppFooter from "../../layout/Footer";
 import { Text } from "@mantine/core";
 import { GradientSpan, Span, Bold } from "../../../toolbox";
@@ -11,7 +11,44 @@ function Welcome(): JSX.Element {
    * all text - is centered in the second with background image
    */
   const [windowHeight] = useState(`${window.innerHeight * 3 + 4}px`);
+  // const [navbarLinks] = useState(document.getElementsByClassName("navbar-links")[0])
+  
 
+  const handleScroll = (offsetY) => {
+    const inner = (e) => {
+      // window.pageYOffset // how much page has been scrolled by Y axis
+    // document.body.offsetHeight // total doc.body height == windowHeight
+    // window.innerHeight // screen height
+    const howMuchUserScrolledPrc = window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
+    // console.log(howMuchUserScrolledPrc)
+    
+    // /* record elements' initial offset from the top */
+    // const offsetY = navbarLinks.getBoundingClientRect().top
+    // console.assert(typeof offsetY === 'number', 'offsetY is expected to be a number')
+    // console.log(offsetY, window.pageYOffset)
+    /* calculate  */
+    const navbarLinks = document.getElementsByClassName("navbar-links")[0]
+    if (window.pageYOffset >= offsetY) {
+      /* stick navbar-links to the top */
+      // console.dir(navbarLinks.getBoundingClientRect())
+      console.log('we need to get sticky!')
+      navbarLinks.style.setProperty('position', 'fixed')
+      navbarLinks.style.setProperty('top', '5px')
+    } else {
+      navbarLinks.style.removeProperty('position')
+      navbarLinks.style.removeProperty('top')
+    }
+    }
+    return inner
+  }
+  /* set up scroll observer */
+  useEffect(() => {
+    const navbarLinks = document.getElementsByClassName("navbar-links")[0]
+    /* record elements' initial offset from the top */
+    const offsetY = navbarLinks.getBoundingClientRect().top
+    console.assert(typeof offsetY === 'number', 'offsetY is expected to be a number')
+    window.addEventListener('scroll', handleScroll(offsetY))
+  }, [])
   return (
     <div className={`welcome w-screen`} style={{ height: windowHeight }}>
       {/* Oda */}
@@ -21,14 +58,20 @@ function Welcome(): JSX.Element {
         </div>
       </section>
 
+      {/* <div className="some-dumb-wrapper">
+      <div className="sticky-div" style={{ position: 'sticky', top: 0 }}>blablablab</div>
+      </div> */}
+
       {/* hello world */}
       <section className="welcome--lorem font-['Inter'] h-1/3 w-full bg-baby-powder flex flex-col justify-center items-center text-gray-900">
         <div className="container-1-4 w-1/4">
+
+          {/* hell world */}
           <div className="mb-32 text-[64px] font-semibold flex flex-row justify-center items-center">
             hello, world!
           </div>
 
-          <div className="mb-16 text-lg font-normal flex flex-col justify-center items-center gap-4">
+          <div className="mb-16 text-lg font-normal flex flex-col justify-center items-center gap-4 relative">
             <div>welcome to my</div>
 
             <div className="navbar-links">
