0af7ed8c4d5a422bb5b7c0c2217864d565c5591b
oda <ooddaa@gmail.com>
2022-05-17 16:15:40 +0100

    welcome page sort of ok

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,24 +1,12 @@
-/* 
-.App-footer p {
-  color: var(--grey);
-  font-size: var(---10);
+import { createStyles } from "@mantine/core";
 
-  padding-top: var(---10);
-  padding-left: var(---10);
-  margin: 0;
-}
-
-.App-footer span.oda {
-  color: var(--amber);
-}
-
-.App-link {
-  color: #61dafb;
-} */
-
-export default function AppFooter() {
+export default function AppFooter({ classes }) {
+  const { cx } = createStyles(() => ({}))();
   return (
-    <div className="App-footer bg-transparent">
+    // <div className="App-footer bg-transparent">
+    <div className={cx(
+      ["App-footer", "bg-transparent", classes]
+    )}>
       <p className="text-gray-500 text-sm p-4"> 
         made with 👾 by <span className="oda text-amber-500">oda</span> in 2022 | photo by{" "}
         <a href="https://unsplash.com/photos/cBhk90BuOeU">Greg Torosiants</a>
--- src/components/pages/Welcome/Welcome.tsx
+++ src/components/pages/Welcome/Welcome.tsx
@@ -13,59 +13,49 @@ function Welcome(): JSX.Element {
 
   return (
     <div className={`welcome w-screen`} style={{ height: windowHeight }}>
+      {/* Oda */}
       <section className="welcome--name h-1/3 w-full bg-white flex justify-center items-center">
         <div className="font-['Italiana'] font-medium text-7xl text-black-500">
-          Oda
+          oda
         </div>
       </section>
 
-      <section className="welcome--lorem font-['Inter'] h-1/3 w-full bg-baby-powder flex flex-col justify-center items-center text-gray-800 ">
-        <div className="welcome--lorem--container w-1/4"> 
-        <div className="mb-24 text-[48px] font-semibold">
-          {/* <Title order={1}>Lorem, world!</Title> */}
-          Lorem, world!
-          My name is <span className="oda font-['Italiana'] text-black">ODA</span>
-        </div>
-        <div className="mb-16 text-lg font-normal">
-              Welcome to my{" "}
-              <GradientSpan from="blue" to="grape">
-                portfolio
-              </GradientSpan>{" "}
+      {/* hello world */}
+      <section className="welcome--lorem font-['Inter'] h-1/3 w-full bg-baby-powder flex flex-col justify-center items-center text-gray-900">
+        <div className="container-1-4 w-1/4">
+          <div className="mb-32 text-[64px] font-semibold flex flex-row justify-center items-center">
+            hello, world!
+          </div>
+
+          {/* <div className="mb-16 text-[32px] font-normal flex flex-row justify-center items-center gap-4">
+            <span>my</span>
+            <span>name</span>
+            <span>is</span>
+            <span className="oda font-['Italiana'] text-[56px] text-black hover:text-amber-500">oda</span>
+          </div> */}
+
+          <div className="mb-16 text-lg font-normal flex flex-row justify-center items-center gap-4">
+            welcome to my{" "}
+            <GradientSpan from="blue" to="grape">
+              portfolio
+            </GradientSpan>{" "}
             <Span color="grey">|</Span>{" "}
-              <GradientSpan from="grape" to="orange">
-                blog
-              </GradientSpan>{" "}
-              <Span color="grey">|</Span>{" "}
-              <GradientSpan from="orange" to="green">
-                playground
-              </GradientSpan>{" "}
-        </div>
-        <div className="mb-4 text-lg font-normal leading-loose">
-              I learn by doing, so every day, after breakfast, I like to have a
-              go at some challenging task: coding/learning psychology/bringing
-              up my lovely son/living here and now and stuff like that.
-            </div>
-        
+            <GradientSpan from="grape" to="orange">
+              blog
+            </GradientSpan>{" "}
+            <Span color="grey">|</Span>{" "}
+            <GradientSpan from="orange" to="green">
+              playground
+            </GradientSpan>{" "}
+          </div>
         </div>
       </section>
 
-      <section className="welcome--lorem font-['Inter'] h-1/3 w-full bg-[#F8FBFD] flex flex-col justify-center items-center">
-      <div className="pb-4">
-              I learn by doing, so every day, after breakfast, I like to have a
-              go at some challenging task: coding/learning psychology/bringing
-              up my lovely son/living here and now and stuff like that.
-            </div>
-      </section>
-
-      <section className="welcome--lorem font-['Inter'] h-1/3 w-full bg-teal-400/50 flex flex-col justify-center items-center">
-
-        
-      </section>
-
-      <section className="welcome--img h-1/3 w-full flex flex-col justify-center items-center">
-        <div className="welcome--text pl-[100px] pr-[100px] mx-auto">
+      {/* image with paralax */}
+      <section className="welcome--img h-1/3 w-full">
+        <div className="welcome-last-text h-5/6 w-full flex flex-col justify-start items-center">
+          <div className="mt-48 container-1-4 w-1/4 ">
           <Text>
-
             <div className="pb-4">
               Over the past decade, working as a{" "}
               <GradientSpan from="green" to="cyan">
@@ -89,15 +79,31 @@ function Welcome(): JSX.Element {
 
             <div className="pb-4">
               <Bold>
-                So feel free to poke around and let me know what you think.
+                Please to poke around and let's chat.
               </Bold>
             </div>
           </Text>
+          </div>
         </div>
-        {/* <div className="lol123 h-full w-full"></div> */}
-        <AppFooter></AppFooter>
+        <div className="welcome-footer h-1/6 flex flex-col justify-end items-center">
+          <AppFooter classes="lol"></AppFooter>
+        </div>
       </section>
     </div>
   );
 }
 export default Welcome;
+
+// {/* <section className="welcome--lorem font-['Inter'] h-1/3 w-full bg-[#F8FBFD] flex flex-col justify-center items-center">
+//         <div className="container-1-4 w-1/4">
+//           <div className="mb-4 text-md font-normal leading-loose">
+//             I learn by doing, so every day, after breakfast, I like to have a go
+//             at some challenging task: coding/learning psychology/bringing up my
+//             lovely son/living here and now and stuff like that.
+//           </div>
+//         </div>
+//       </section>
+
+//       <section className="welcome--lorem font-['Inter'] h-1/3 w-full bg-teal-100/50 flex flex-col justify-center items-center"></section> */}
+
+ 
