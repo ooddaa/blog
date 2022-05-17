d2268c2e1c16c8a0169f327ec20e5d0c3a90c514
oda <ooddaa@gmail.com>
2022-05-17 16:22:13 +0100

    added links to portfolio|blog|playground. now need to make them move on top when scroll down

--- src/components/pages/Welcome/Welcome.tsx
+++ src/components/pages/Welcome/Welcome.tsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import AppFooter from "../../layout/Footer";
-import { Text, Title } from "@mantine/core";
+import { Text } from "@mantine/core";
 import { GradientSpan, Span, Bold } from "../../../toolbox";
+import { Link } from "react-router-dom";
 
 function Welcome(): JSX.Element {
   /**
@@ -13,6 +14,7 @@ function Welcome(): JSX.Element {
 
   return (
     <div className={`welcome w-screen`} style={{ height: windowHeight }}>
+
       {/* Oda */}
       <section className="welcome--name h-1/3 w-full bg-white flex justify-center items-center">
         <div className="font-['Italiana'] font-medium text-7xl text-black-500">
@@ -27,26 +29,25 @@ function Welcome(): JSX.Element {
             hello, world!
           </div>
 
-          {/* <div className="mb-16 text-[32px] font-normal flex flex-row justify-center items-center gap-4">
-            <span>my</span>
-            <span>name</span>
-            <span>is</span>
-            <span className="oda font-['Italiana'] text-[56px] text-black hover:text-amber-500">oda</span>
-          </div> */}
-
           <div className="mb-16 text-lg font-normal flex flex-row justify-center items-center gap-4">
             welcome to my{" "}
-            <GradientSpan from="blue" to="grape">
-              portfolio
-            </GradientSpan>{" "}
+            <a href="https://github.com/ooddaa">
+              <GradientSpan from="blue" to="grape">
+                portfolio
+              </GradientSpan>
+            </a>{" "}
             <Span color="grey">|</Span>{" "}
-            <GradientSpan from="grape" to="orange">
-              blog
-            </GradientSpan>{" "}
+            <Link to="/blog">
+              <GradientSpan from="grape" to="orange">
+                blog
+              </GradientSpan>
+            </Link>{" "}
             <Span color="grey">|</Span>{" "}
-            <GradientSpan from="orange" to="green">
-              playground
-            </GradientSpan>{" "}
+            <Link to="/playground">
+              <GradientSpan from="orange" to="green">
+                playground
+              </GradientSpan>
+            </Link>
           </div>
         </div>
       </section>
@@ -55,34 +56,32 @@ function Welcome(): JSX.Element {
       <section className="welcome--img h-1/3 w-full">
         <div className="welcome-last-text h-5/6 w-full flex flex-col justify-start items-center">
           <div className="mt-48 container-1-4 w-1/4 ">
-          <Text>
-            <div className="pb-4">
-              Over the past decade, working as a{" "}
-              <GradientSpan from="green" to="cyan">
-                Business Developer
-              </GradientSpan>{" "}
-              /{" "}
-              <GradientSpan from="red" to="orange">
-                Software Engineer
-              </GradientSpan>{" "}
-              I have accumulated a bunch of googlesheets, notes, business ideas
-              and projects that I would like to keep playing with.
-            </div>
+            <Text>
+              <div className="pb-4">
+                Over the past decade, working as a{" "}
+                <GradientSpan from="green" to="cyan">
+                  Business Developer
+                </GradientSpan>{" "}
+                /{" "}
+                <GradientSpan from="red" to="orange">
+                  Software Engineer
+                </GradientSpan>{" "}
+                I have accumulated a bunch of googlesheets, notes, business
+                ideas and projects that I would like to keep playing with.
+              </div>
 
-            <div className="pb-4">
-              My <a href="https://github.com/med4kat">wife</a> is amazing and we
-              have a wonderful toddler son ☺️
-            </div>
-            <div className="pb-4">
-              I also like <a href="https://www.wavebjj.co/">BJJ</a>.
-            </div>
+              <div className="pb-4">
+                My <a href="https://github.com/med4kat">wife</a> is amazing and
+                we have a wonderful toddler son ☺️
+              </div>
+              <div className="pb-4">
+                I also like <a href="https://www.wavebjj.co/">BJJ</a>.
+              </div>
 
-            <div className="pb-4">
-              <Bold>
-                Please to poke around and let's chat.
-              </Bold>
-            </div>
-          </Text>
+              <div className="pb-4">
+                <Bold>Please to poke around and let's chat.</Bold>
+              </div>
+            </Text>
           </div>
         </div>
         <div className="welcome-footer h-1/6 flex flex-col justify-end items-center">
@@ -94,16 +93,3 @@ function Welcome(): JSX.Element {
 }
 export default Welcome;
 
-// {/* <section className="welcome--lorem font-['Inter'] h-1/3 w-full bg-[#F8FBFD] flex flex-col justify-center items-center">
-//         <div className="container-1-4 w-1/4">
-//           <div className="mb-4 text-md font-normal leading-loose">
-//             I learn by doing, so every day, after breakfast, I like to have a go
-//             at some challenging task: coding/learning psychology/bringing up my
-//             lovely son/living here and now and stuff like that.
-//           </div>
-//         </div>
-//       </section>
-
-//       <section className="welcome--lorem font-['Inter'] h-1/3 w-full bg-teal-100/50 flex flex-col justify-center items-center"></section> */}
-
- 
