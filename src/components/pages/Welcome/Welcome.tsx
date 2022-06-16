79fcd84f91faac9bcc30be2e25287299ddb514b5
oda <ooddaa@gmail.com>
2022-06-16 10:41:07 +0100

    [Welcome] simplified, responsive

--- src/components/pages/Welcome/Welcome.tsx
+++ src/components/pages/Welcome/Welcome.tsx
@@ -13,80 +13,80 @@ function Welcome(): JSX.Element {
    */
   const [windowHeight] = useState(`${window.innerHeight * 3 + 4}px`);
 
-  const handleScroll = (offsetY: number) => {
-    const inner = (e: Event) => {
-      /* window.pageYOffset // how much page has been scrolled by Y axis */
-      /* window.innerHeight // screen height == document.body.offsetHeight */
+  // const handleScroll = (offsetY: number) => {
+  //   const inner = (e: Event) => {
+  //     /* window.pageYOffset // how much page has been scrolled by Y axis */
+  //     /* window.innerHeight // screen height == document.body.offsetHeight */
 
-      /* calculate  */
-      const navbarLinks: any =
-        document.getElementsByClassName("navbar-links")[0];
+  //     /* calculate  */
+  //     const navbarLinks: any =
+  //       document.getElementsByClassName("navbar-links")[0];
 
-      // console.table({
-      //   pageYOffset: window.pageYOffset,
-      //   innerHeight: window.innerHeight,
-      //   scrolled: window.pageYOffset / window.innerHeight,
-      //   storedOffsetY: offsetY,
-      //   realOffsetY: navbarLinks.getBoundingClientRect().top
-      // })
+  //     // console.table({
+  //     //   pageYOffset: window.pageYOffset,
+  //     //   innerHeight: window.innerHeight,
+  //     //   scrolled: window.pageYOffset / window.innerHeight,
+  //     //   storedOffsetY: offsetY,
+  //     //   realOffsetY: navbarLinks.getBoundingClientRect().top
+  //     // })
 
-      if (window.pageYOffset >= offsetY) {
-        /* stick navbar-links to the top */
-        navbarLinks?.style?.setProperty("position", "fixed");
-        navbarLinks?.style?.setProperty("top", "5px");
-      } else {
-        navbarLinks?.style?.removeProperty("position");
-        navbarLinks?.style?.removeProperty("top");
-      }
-    };
-    return inner;
-  };
+  //     if (window.pageYOffset >= offsetY) {
+  //       /* stick navbar-links to the top */
+  //       navbarLinks?.style?.setProperty("position", "fixed");
+  //       navbarLinks?.style?.setProperty("top", "5px");
+  //     } else {
+  //       navbarLinks?.style?.removeProperty("position");
+  //       navbarLinks?.style?.removeProperty("top");
+  //     }
+  //   };
+  //   return inner;
+  // };
 
   /* init */
-  useEffect(() => {
+  // useEffect(() => {
     /* scroll to top */
     // window.scrollTo({ top: 0, behavior: "smooth" });
 
-    const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+    // const navbarLinks = document.getElementsByClassName("navbar-links")[0];
 
-    /* save elements' initial offset from the top to persist in localStorage*/
-    if (!localStorage.getItem("offsetY")) {
-      /* save initial offset */
-      const offsetY = navbarLinks.getBoundingClientRect().top;
-      localStorage.setItem("offsetY", JSON.stringify(offsetY));
+    // /* save elements' initial offset from the top to persist in localStorage*/
+    // if (!localStorage.getItem("offsetY")) {
+    //   /* save initial offset */
+    //   const offsetY = navbarLinks.getBoundingClientRect().top;
+    //   localStorage.setItem("offsetY", JSON.stringify(offsetY));
 
-      /* remember window size */
-      localStorage.setItem("windowSize", JSON.stringify(window.innerHeight));
-    }
+    //   /* remember window size */
+    //   localStorage.setItem("windowSize", JSON.stringify(window.innerHeight));
+    // }
 
-    /* update if user has resized window */
-    const oldWindow = JSON.parse(localStorage.getItem("windowSize"));
-    const currentWindow = window.innerHeight;
+    // /* update if user has resized window */
+    // const oldWindow = JSON.parse(localStorage.getItem("windowSize"));
+    // const currentWindow = window.innerHeight;
 
-    if (oldWindow !== currentWindow) {
-      console.log("oldWindow !== currentWindow");
+    // if (oldWindow !== currentWindow) {
+    //   console.log("oldWindow !== currentWindow");
 
-      /* adjust offsetY */
-      localStorage.removeItem("offsetY");
-      /* pay attention - if the window was resized whilst not being scrolled up to the top - add pageYOffset  */
-      localStorage.setItem(
-        "offsetY",
-        JSON.stringify(
-          navbarLinks.getBoundingClientRect().top + window.pageYOffset
-        )
-      );
+    //   /* adjust offsetY */
+    //   localStorage.removeItem("offsetY");
+    //   /* pay attention - if the window was resized whilst not being scrolled up to the top - add pageYOffset  */
+    //   localStorage.setItem(
+    //     "offsetY",
+    //     JSON.stringify(
+    //       navbarLinks.getBoundingClientRect().top + window.pageYOffset
+    //     )
+    //   );
 
-      /* remember window size */
-      localStorage.removeItem("windowSize");
-      localStorage.setItem("windowSize", JSON.stringify(window.innerHeight));
-    }
+    //   /* remember window size */
+    //   localStorage.removeItem("windowSize");
+    //   localStorage.setItem("windowSize", JSON.stringify(window.innerHeight));
+    // }
 
-    /* set up scroll observer */
-    window.addEventListener(
-      "scroll",
-      handleScroll(JSON.parse(localStorage.getItem("offsetY")))
-    );
-  }, []);
+    // /* set up scroll observer */
+    // window.addEventListener(
+    //   "scroll",
+    //   handleScroll(JSON.parse(localStorage.getItem("offsetY")))
+    // );
+  // }, []);
 
   return (
     <div className={`welcome w-screen`} style={{ height: windowHeight }}>
@@ -99,7 +99,7 @@ function Welcome(): JSX.Element {
 
       {/* hello world */}
       <section className="welcome--lorem font-['Italiana'] h-1/3 w-full bg-baby-powder flex flex-col justify-center items-center text-gray-900">
-        <div className="container-1-4 w-1/4">
+        <div className="container-1-4 w-3/4">
           {/* hell world */}
           <div
             className="mb-32 text-[64px] font-semibold flex flex-row justify-center items-center"
@@ -108,8 +108,8 @@ function Welcome(): JSX.Element {
             {"Hello, World!"}
           </div>
 
-          <div className="mb-16 text-[20px] flex flex-col justify-center items-center gap-4 relative">
-            <span>welcome to my</span>
+          <div className="mb-16 font-['Inter'] text-[18px] font-medium flex flex-col justify-center items-center gap-4 relative">
+            {/* <span>welcome to my</span> */}
 
             <div className="navbar-links">
               <Link to="/blog">
@@ -136,8 +136,14 @@ function Welcome(): JSX.Element {
 
       {/* bio */}
       <section className="bio">
-        <section className="welcome-last-text bg-[#F9FAFD] h-5/6 w-full flex flex-col justify-start items-center">
-          <Text size="lg" className=" m-48 px-36 font-['Inter']">
+        <div className="welcome-last-text bg-[#F9FAFD] h-5/6 w-full flex flex-col justify-start items-center">
+          <Text size="lg" className={`
+          my-12 mx-8 px-8 
+          sm:md-16 sm:px-16 
+          md:mx-20 md:px-20 
+          lg:mx-32 lg:px-32
+          xl:mx-48 xl:px-48 
+          text-sm sm:text-lg sm:leading-8 font-['Inter']`}>
             <div className="pb-4">
               Over the past decade, working as a{" "}
               <GradientSpan from="green" to="cyan">
@@ -176,7 +182,7 @@ function Welcome(): JSX.Element {
               I also like <a href="https://www.wavebjj.co/">BJJ</a>.
             </div>
           </Text>
-        </section>
+        </div>
       </section>
       {/* image with paralax */}
       <section className="welcome--img bg-center h-1/5 w-full">
